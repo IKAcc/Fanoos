e2273d71064caf4de3732551666e68b838c52352
Javid Izadfar <javid.izadfar@gmail.com>
2016-08-19 10:59:46 +0430

    MVP script
    fanoos is now just working. using button.

--- fanoos.js
+++ fanoos.js
@@ -1,26 +1,8 @@
-var elementList = [];
-
 function init() {
 
-  var elementNumber = 0;
-
   $('.fanoos').each(function() {
-
     $(this).addClass('animated').css("animation-play-state", "paused");
-
-    if ( $(this).attr('data-fanoos-id') == null || $(this).attr('data-fanoos-id').length == 0) {
-      var fanoosId = 'fanoosElement' + elementNumber;
-      $(this).attr('data-fanoos-id', fanoosId);
-    } else {
-      var fanoosId = $(this).attr('data-fanoos-id');
-    };
-
-    console.log(fanoosId)
-    console.log($(this).offset().top);
-
-    elementNumber++;
   })
-
 
 }
 
@@ -30,28 +12,10 @@ function action(element) {
 
 init()
 
-$('#action1').on('click', function(e) {
+$('#action').on('click', function(e) {
 
   var element = $('.fadeInLeft');
 
   action(element);
-  e.preventDefault();
-})
-
-
-$('#action2').on('click', function(e) {
-
-    var element = $('.tada');
-
-    action(element);
-  e.preventDefault();
-})
-
-
-$('#action3').on('click', function(e) {
-
-    var element = $('.fadeInRight');
-
-    action(element);
   e.preventDefault();
 })
